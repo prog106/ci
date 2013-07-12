7b7c00400cdddd051b686410a63aa0da509ba3ba
prog106 <prog106@gmail.com>
2013-07-12 14:57:46 +0900

    complete

--- application/views/macham/view.php
+++ application/views/macham/view.php
@@ -1,6 +1,5 @@
                 <li id="view">
                     <div class="fullcomment">
-                        <?=($row['mu_title'])? $row['mu_title'].'<br>' : '';?>
                         <pre><?=$row['mu_comment'];?></pre>
                         <?=((!empty($row['mu_imagesrc']))? '<img src="/static/upload/'.$row['mu_imagesrc'].'">' : '');?>
                     </div>
