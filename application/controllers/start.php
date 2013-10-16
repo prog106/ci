459214c0170e933396f6f076d268aa96462fd469
prog106 <prog106@gmail.com>
2013-10-16 19:17:34 +0900

    baby photo start

--- application/controllers/start.php
+++ application/controllers/start.php
@@ -1,9 +1,10 @@
 <?php
 class Start extends CI_Controller {
     function index() {
-        header('Location: /adform');
-        header('Location: /mumu');
-        header('Location: /macham');
+//        header('Location: /adform');
+//        header('Location: /mumu');
+//        header('Location: /macham');
+        header('Location: /baby');
 //        header('Location: /mysp');
     }
 }
